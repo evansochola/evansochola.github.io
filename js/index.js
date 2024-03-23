a9688a5afc676253b6c587778c94d7b61ca203be
Evans Ochola <evansochola14@gmail.com>
2024-03-23 18:49:26 +0000

    Add js routing Fix: responsive issues Add about page

--- js/index.js
+++ js/index.js
@@ -2,29 +2,28 @@ function sidebar(){
    var sidebarElement = document.getElementById("sidebar")
    var bodyElement = document.getElementById("body")
    var landingPage = document.getElementById("landing-page")
+   var pageWrapper = document.getElementById("page-wrapper")
 
    //desktop
    if (window.screen.width >= 1024 && window.screen.height >= 768) {
      if(sidebarElement.style.display == "none") {
           sidebarElement.style.display = "block";
-          bodyElement.style.overflow = 'hidden'; 
      }
      else {
           sidebarElement.style.display = "none";
-          bodyElement.style.overflow="auto";
      }
    }
    else {
         if (sidebarElement.style.display == "none") {
              
              bodyElement.style.overflow = 'hidden'; 
-             landingPage.style.visibility = "hidden";
+             pageWrapper.style.visibility = "hidden";
              sidebarElement.style.display="block";
           }
           else {
                bodyElement.style.overflow="auto";
+               pageWrapper.style.visibility = "visible";
                sidebarElement.style.display="none";
-               landingPage.style.visibility = "visible";
           }
    }
 }
